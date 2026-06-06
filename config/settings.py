9f3ea9a7d6f0097cb8fb2db5429acd7d479f1df0
Bhoja <49086758+Gurucharan-G@users.noreply.github.com>
2026-06-06 15:54:30 +0530

    Allow all hosts for tunneling

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4smj8x#l)v#otmxlqf7!0!oasv$3m9*=g@jovzs&a&8dx57)%b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
