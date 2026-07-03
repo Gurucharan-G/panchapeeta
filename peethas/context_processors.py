8fc734c1058cbb035042e1d13fb11ef8594115f2
Bhoja <49086758+Gurucharan-G@users.noreply.github.com>
2026-07-03 22:56:09 +0530

    Implement hover switcher dropdown for active Peetha in main nav bar listing other Peethas

--- peethas/context_processors.py
+++ peethas/context_processors.py
@@ -3,11 +3,13 @@ from .feature_flags import DEVOTEE_REGISTRATION, DEVOTIONAL_CHANTING
 
 def live_peethas_processor(request):
     """
-    Returns a list of Peethas that currently have an active live stream.
+    Returns a list of Peethas that currently have an active live stream, all Peethas, and feature flags.
     """
     active_live = Peetha.objects.filter(live_youtube_url__isnull=False).exclude(live_youtube_url='')
+    all_peethas = Peetha.objects.all().order_by('id')
     return {
         'live_peethas': active_live,
+        'all_peethas': all_peethas,
         'devotee_registration_enabled': bool(DEVOTEE_REGISTRATION),
         'devotional_chanting_enabled': bool(DEVOTIONAL_CHANTING),
     }
